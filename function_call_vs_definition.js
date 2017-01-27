781630fa57a50094943dd60f518755b9ec3c41a6
Hajar Mokhtafa <hajarmokhtafa@gmail.com>
2017-01-27 15:31:05 +0100

    removed two files

--- function_call_vs_definition.js
+++
@@ -1,8 +0,0 @@
-
-//function definitie
-function ditDoetWeinig(ditIsEenParameter){
-	console.log(' :) ' + ditIsEenParameter);
-} // <-- te zien aan het einde van een curly brace {
-
-//function call
-ditDoetWeinig("Dit is een argument en deze dingen hebben echte inhoud");
